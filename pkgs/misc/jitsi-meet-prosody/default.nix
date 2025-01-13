2f8c4330b83608ee0bd10f3256b5fc2428188d4c
tuedel <tuedel@tdl.gr>
2025-01-14 00:52:33 +0100

    jitsi-meet-prosody: 1.0.8302 -> 1.0.8302

--- pkgs/misc/jitsi-meet-prosody/default.nix
+++ pkgs/misc/jitsi-meet-prosody/default.nix
@@ -8,10 +8,10 @@
 
 stdenv.mkDerivation rec {
   pname = "jitsi-meet-prosody";
-  version = "1.0.8302";
+  version = "1.0.8339";
   src = fetchurl {
     url = "https://download.jitsi.org/stable/${pname}_${version}-1_all.deb";
-    sha256 = "CWAunopJ49LnEV9D12+rZ6pFbeN0oeJgtwNrGSpfj2k=";
+    sha256 = "J2O9ItSmVqRBHFt41bp2PfBgoPPiTFO/rcz603CjObE=";
   };
 
   nativeBuildInputs = [ dpkg ];
