b7fac74a420f657c210a51867398e08ad6ef7f40
tuedel <tuedel@tdl.gr>
2025-01-18 18:40:32 +0100

    jicofo: 1.0-1117 -> 1.0-1118

--- pkgs/servers/jicofo/default.nix
+++ pkgs/servers/jicofo/default.nix
@@ -9,10 +9,10 @@
 
 let
   pname = "jicofo";
-  version = "1.0-1117";
+  version = "1.0-1118";
   src = fetchurl {
     url = "https://download.jitsi.org/stable/${pname}_${version}-1_all.deb";
-    sha256 = "PUIPLCdL68+cf3eue7+p0aptYavq046RtSaFM9XPgQQ=";
+    sha256 = "teOndy0tMtO5Mkalll4/epp1Ci8FgGyIyeELKCgSqFU=";
   };
 in
 stdenv.mkDerivation {
